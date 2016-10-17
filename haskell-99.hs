d336bce8c8d7953122444e2906d209f525d4d382
ioantm <ioan.tomulesei@ayonesoftware.com>
2016-10-17 20:52:38 +0300

    p3

--- haskell-99.hs
+++ haskell-99.hs
@@ -1,2 +1,8 @@
 myLast =  head . reverse
 myButLast = head . drop 1 . reverse
+
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "empty list"
+elementAt xs n
+  | n < 1 = error "Index out of bounds"
+  | otherwise = last . take n $ xs
