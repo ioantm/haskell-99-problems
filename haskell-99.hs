a029a3fbae621bf4e54b742ccbd495aad4d36f53
ioantm <ioan.tomulesei@yahoo.com>
2016-11-09 22:30:37 +0200

    continued with problems

--- haskell-99.hs
+++ haskell-99.hs
@@ -69,3 +69,52 @@ encode' :: Eq a => [a] -> [Encoded a]
 encode' = map(\x -> encodePack x) . pack
   where encodePack (x:[]) = Single x
         encodePack xx@(x:xs) = Multiple (length xx) x
+
+--p13
+
+addToEncode :: Eq a => a -> [Encoded a] -> [Encoded a]
+addToEncode a [] = [Single a]
+addToEncode a (x@(Single b):xs) = if a == b
+                                    then (Multiple 2 a):xs
+                                    else (Single a) : x : xs
+addToEncode a (x@(Multiple n b) : xs) = if a == b
+                                          then (Multiple (n + 1) b) : xs
+                                          else (Single a) : x : xs
+
+encodeDirect :: Eq a => [a] -> [Encoded a]
+encodeDirect = foldr addToEncode []
+
+--p14
+
+dupli :: [a] -> [a]
+dupli [] = []
+dupli (x:xs) = x:x:(dupli xs)
+
+--p15
+repli :: [a] -> Int -> [a]
+repli [] _ = []
+repli (x:xs) n = ((take n) . repeat $ x) ++ (repli xs n)
+
+--p16
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery xs n = (take (n-1) xs) ++ (dropEvery (drop n xs) n)
+
+--p17
+split :: [a] -> Int -> ([a], [a])
+-- split [] _ = ([], [])
+-- split xs 0 = ([], xs)
+-- split xs n = (take n xs, drop n xs)
+
+split [] _ = ([], [])
+split xs 0 = ([], xs)
+split (x: xs) n = let (a, b) = split xs (n - 1)
+                  in (x:a, b)
+
+-- split xs n = foldl (\acc@(first, second) x -> if (length first) == n
+--                                 then (first, second ++ [x])
+--                                 else (first ++ [x], second)) ([], []) xs
+
+--p18
+slice :: [a] -> Int -> Int -> [a]
+slice xs start end = fst (split (snd (split xs (start - 1))) (end - start + 1))
